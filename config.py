4fc4a1a972138aa82de891066672f4e8eb11d310
vinculofreefire94-art <vinculofreefire94@gmail.com>
2026-05-04 15:26:24 +0000

    Add configuration parameters to config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+GRUPO_ID = -1003796977029
+
+MAX_HISTORICO = 200
+SIMILARIDADE_LIMITE = 0.85
+TEMPO_LIMITE = 120
+
+# sistema de avisos (para próximo módulo)
+MAX_WARNINGS = 3

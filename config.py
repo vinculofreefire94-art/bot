ad4a9521ed36b7ba7ca8f18389d046ed5caa55b0
vinculofreefire94-art <vinculofreefire94@gmail.com>
2026-05-04 18:50:59 +0000

    Add configuration settings in config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+GRUPO_ID = -1003796977029
+
+# anti-spam
+SIMILARIDADE_LIMITE = 0.85
+TEMPO_LIMITE = 120
+MAX_HISTORICO = 200
+
+# sistema de punição
+MAX_WARNINGS = 3
+
+# crescimento
